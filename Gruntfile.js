b9b5ea2902bb3fc75f74ad8f77316d43e2619abf
Tri Truong <tri.itvn@gmail.com>
2016-04-15 18:39:07 +0700

    Init project

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      main: {
+        files: {
+          './build/css/main.css': './src/sass/main.scss',
+        }
+      },
+      bootstrap: {
+        files: {
+          './build/css/customized_bootstrap.css': './src/sass/customized_bootsrap.scss',
+        },
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ['src/sass/**/*.scss'],
+        tasks: ['sass:main'],
+      },
+    },
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+            './build/css/*.css',
+            './build/*.html'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: './build'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('serve', [
+    'browserSync',
+    'watch'
+  ]);
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-browser-sync');
+};
